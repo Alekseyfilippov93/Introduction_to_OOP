ce8c4013e6d8c978ad46f70d30ac02dff7ae13a8
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-07-16 01:55:52 +0700

    Обновление кода. Написание readme

--- src/category.py
+++ src/category.py
@@ -1,15 +1,31 @@
+from src.product import Product
+
+
 class Category:
     """Определяем для каждого свой тип данных"""
 
     name: str
     description: str
-    products: list
+    __products: list  # Приватный доступ к атрибуту
     category_count = 0
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products  # Список товаров с приватным доступом
         Category.category_count += 1
         Category.product_count = len(self.products)
+
+    @property
+    def products(self) -> str:
+        "Геттер для списка товаров"
+        products_list = []
+        for product in self.__products:
+            products_list.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+        return "\n".join(products_list)
+
+    def add_product(self, product: Product):
+        "Метод добавления товаров"
+        self.__products.append(product)
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -2,5 +2,28 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name  # Название
         self.description = description  # Описание
-        self.price = price  # Цена
+        self.__price = price  # Приватный атрибут цены.
         self.quantity = quantity  # Количество в наличии
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        """Классметод для создания продукта из словаря"""
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
+
+    @property
+    def price(self) -> float:
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер цены на положительное значение"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
