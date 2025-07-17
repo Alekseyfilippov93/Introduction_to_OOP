cf1af4d6792a1aa78959ac4fd599beb0c370d91a
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-07-17 18:28:04 +0700

    Доработан модуль категория по замечаниям

--- src/category.py
+++ src/category.py
@@ -11,23 +11,34 @@ class Category:
     product_count: int = 0
 
     def __init__(self, name: str, description: str, products: list) -> None:
-        self.name = name
-        self.description = description
-        self.__products = products  # Список товаров с приватным доступом
+        self.name = name  # Название категории
+        self.description = description  # Описание категории
+        self.__products = []  # Список товаров с приватным доступом
         Category.category_count += 1
         Category.product_count = len(products)
 
+        for product in products:
+            self.add_product(product)
+
     @property
     def products(self) -> str:
-        "Геттер для списка товаров"
-        products_list = []
-        for product in self.__products:
-            products_list.append(
-                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
-            )
-        return "\n".join(products_list)
+        """Геттер для списка товаров"""
+        return "\n".join(
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            for product in self.__products
+        )
 
     def add_product(self, product: Product):
-        "Метод добавления товаров"
+        """Метод добавления товаров"""
+        if not isinstance(product, Product):
+            raise TypeError("Добавляем только Product")
         self.__products.append(product)
         Category.product_count += 1
+
+    def __leb__(self):
+        """Считаем количество товаров"""
+        return len(self.__products)
+
+    def __str__(self):
+        """Строчное представление категории"""
+        return f"{self.name}, количество продуктов: {len(self)} шт."
